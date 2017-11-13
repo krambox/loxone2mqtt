e5659f0dedde5307f97241eecd6891db738aead7
Kai Kramer <kai@gaulstall.de>
2017-11-13 01:16:49 +0100

    semistandard

--- bin/lox-mqtt-gateway.js
+++ bin/lox-mqtt-gateway.js
@@ -1,69 +1,69 @@
 #!/usr/bin/env node
 
-const lox_mqtt_gateway = require('../lib/index.js');
+const loxMqttGateway = require('../lib/index.js');
 
-if (!process.env.NODE_CONFIG_DIR){
-    process.env.NODE_CONFIG_DIR = __dirname+"/../config/";
+if (!process.env.NODE_CONFIG_DIR) {
+  process.env.NODE_CONFIG_DIR = __dirname + '/../config/';
 }
-var config = require("config");
+var config = require('config');
 
-var logger = lox_mqtt_gateway.Logger(config.get('winston'));
-var app = new lox_mqtt_gateway.App(logger);
-var mqtt_client = lox_mqtt_gateway.mqtt_builder(config.get('mqtt'), app);
-var lox_client = lox_mqtt_gateway.WebSocketAPI(config.get('miniserver'), app);
-var lox_mqtt_adaptor = undefined;
+var logger = loxMqttGateway.Logger(config.get('winston'));
+var app = new loxMqttGateway.App(logger);
+var mqttClient = loxMqttGateway.mqtt_builder(config.get('mqtt'), app);
+var loxClient = loxMqttGateway.WebSocketAPI(config.get('miniserver'), app);
+var loxMqttAdaptor;
 
-app.on('exit', function(code) {
-    if (lox_mqtt_adaptor){
-        lox_mqtt_adaptor.abort();
-    }
+app.on('exit', function (code) {
+  if (loxMqttAdaptor) {
+    loxMqttAdaptor.abort();
+  }
 });
 
-function _update_event(uuid, value){
-    if (lox_mqtt_adaptor){
-        lox_mqtt_adaptor.set_value_for_uuid(uuid, value);
+function updateEvent (uuid, value) {
+  if (loxMqttAdaptor) {
+    loxMqttAdaptor.set_value_for_uuid(uuid, value);
+  }
+}
+
+loxClient.on('update_event_text', updateEvent);
+loxClient.on('update_event_value', updateEvent);
+
+loxClient.on('get_structure_file', function (data) {
+  if (loxMqttAdaptor) {
+    loxMqttAdaptor.abort();
+  }
+
+  loxMqttAdaptor = new loxMqttGateway.Adaptor(loxMqttGateway.Structure.create_from_json(data,
+    function (value) {
+      logger.warn('MQTT Structure - invalid type of control', value);
     }
-};
+  ));
 
-lox_client.on('update_event_text', _update_event);
-lox_client.on('update_event_value', _update_event);
+  mqttClient.subscribe(loxMqttAdaptor.get_topic_for_subscription());
 
-lox_client.on('get_structure_file', function(data) {
-    if (lox_mqtt_adaptor){
-        lox_mqtt_adaptor.abort();
-    }
-
-    lox_mqtt_adaptor = new lox_mqtt_gateway.Adaptor(lox_mqtt_gateway.Structure.create_from_json(data,
-        function(value) {
-            logger.warn("MQTT Structure - invalid type of control", value);
-        }
-    ));
-
-    mqtt_client.subscribe(lox_mqtt_adaptor.get_topic_for_subscription());
-
-    lox_mqtt_adaptor.on('for_mqtt', function(topic, data){
-        logger.debug("MQTT Adaptor - for mqtt: ", {topic: topic, data: data});
-        mqtt_client.publish(topic, data);
-    });
+  loxMqttAdaptor.on('for_mqtt', function (topic, data) {
+    logger.debug('MQTT Adaptor - for mqtt: ', {topic: topic, data: data});
+    mqttClient.publish(topic, data);
+  });
 });
 
-mqtt_client.on('connect', function(conack){
-    if (!lox_client.is_connected()){
-        lox_client.connect();
-    }
+mqttClient.on('connect', function (conack) {
+  if (!loxClient.is_connected()) {
+    loxClient.connect();
+  }
 });
 
-mqtt_client.on('message', function(topic, message, packet) {
-    if (!lox_mqtt_adaptor){
-        return;
-    }
-    var action = lox_mqtt_adaptor.get_command_from_topic(topic, message.toString());
+mqttClient.on('message', function (topic, message, packet) {
+  if (!loxMqttAdaptor) {
+    return;
+  }
+  var action = loxMqttAdaptor.get_command_from_topic(topic, message.toString());
 
-    app.logger.debug("MQTT Adaptor - for miniserver: ", {uuidAction: action.uuidAction, command: action.command});
+  app.logger.debug('MQTT Adaptor - for miniserver: ', {uuidAction: action.uuidAction, command: action.command});
 
-    if (!config.miniserver.readonly){
-        lox_client.send_cmd(action.uuidAction, action.command);
-    }else{
-        app.logger.debug("MQTT Adaptor - readonly mode");
-    }
+  if (!config.miniserver.readonly) {
+    loxClient.send_cmd(action.uuidAction, action.command);
+  } else {
+    app.logger.debug('MQTT Adaptor - readonly mode');
+  }
 });
